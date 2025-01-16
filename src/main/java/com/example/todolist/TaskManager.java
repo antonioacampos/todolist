eac7769d9cf3ec215d20e98404c06a7ac3b4bfd8
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-01-16 08:28:43 -0300

    methods added to the task manager

--- src/main/java/com/example/todolist/TaskManager.java
+++ src/main/java/com/example/todolist/TaskManager.java
@@ -9,4 +9,37 @@ public class TaskManager {
     public TaskManager() {
         this.tasks = new ArrayList<Task>();
     }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void listTasks(){
+        if(tasks.isEmpty()){
+            System.out.println("Lista de tarefas vazia!");
+        } else {
+            tasks.stream().forEach(task -> {System.out.println(task);});
+        }
+    }
+
+    public void removeTask(int index){
+        if(index >= tasks.size() || index < 0){
+            System.out.println("Indíce inválido");
+        } else {
+            tasks.remove(index);
+        }
+    }
+
+    public void removeAllTasks(){
+        tasks.clear();
+    }
+
+    public void updateTaskStatus(int index, String status){
+        if(index >= tasks.size() || index < 0){
+            tasks.get(index).setStatus(status);
+            System.out.println("Status atualizado com sucesso!");
+        } else {
+            System.out.println("Índice inválido!");
+        }
+    }
 }
