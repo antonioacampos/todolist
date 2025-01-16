6a77261a7f4455ad69d65204dd71e373d6480b85
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-01-16 09:21:57 -0300

    main finished - part 1

--- src/main/java/com/example/todolist/TaskManager.java
+++ src/main/java/com/example/todolist/TaskManager.java
@@ -18,7 +18,7 @@ public class TaskManager {
         if(tasks.isEmpty()){
             System.out.println("Lista de tarefas vazia!");
         } else {
-            tasks.stream().forEach(task -> {System.out.println(task);});
+            tasks.stream().forEach(task -> {System.out.println((tasks.indexOf(task) + 1) + ". " + task);});
         }
     }
 
@@ -36,10 +36,10 @@ public class TaskManager {
 
     public void updateTaskStatus(int index, String status){
         if(index >= tasks.size() || index < 0){
+            System.out.println("Índice inválido!");
+        } else {
             tasks.get(index).setStatus(status);
             System.out.println("Status atualizado com sucesso!");
-        } else {
-            System.out.println("Índice inválido!");
         }
     }
 }
--- src/main/java/com/example/todolist/TodolistApplication.java
+++ src/main/java/com/example/todolist/TodolistApplication.java
@@ -1,13 +1,70 @@
 package com.example.todolist;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
-@SpringBootApplication
 public class TodolistApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodolistApplication.class, args);
+        TaskManager manager = new TaskManager();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n1. Adicionar nova tarefa");
+            System.out.println("2. Listar tarefas");
+            System.out.println("3. Atualizar status de tarefa");
+            System.out.println("4. Remover tarefa");
+            System.out.println("5. Limpar lista");
+            System.out.println("0. Sair");
+
+            System.out.println("Digite a opção desejada: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("\nDigite o título: ");
+                    String titulo = scanner.nextLine();
+                    System.out.println("\nDigite a descrição: ");
+                    String descricao = scanner.nextLine();
+                    System.out.println("\nDigite o status atual (pendente, em andamento, concluída): ");
+                    String status = scanner.nextLine();
+                    System.out.println("\nDigite o prazo: ");
+                    String deadline = scanner.nextLine();
+
+                    Task task = new Task(titulo, descricao, status, deadline);
+                    manager.addTask(task);
+                    break;
+                    case 2:
+                        manager.listTasks();
+                        break;
+                    case 3:
+                        System.out.println("Digite o índice da tarefa: ");
+                        int idUpdate = scanner.nextInt() - 1;
+                        System.out.println("\nDigite o novo status da tarefa: ");
+                        String statusUpdate = scanner.nextLine();
+                        manager.updateTaskStatus(idUpdate, statusUpdate);
+                        break;
+                    case 4:
+                        System.out.println("Digite o índice da tarefa: ");
+                        int idRemove = scanner.nextInt() - 1;
+                        manager.removeTask(idRemove);
+                        break;
+                    case 5:
+                        System.out.println("Confirmar limpeza da lista de tarefas (S/n): ");
+                        String confirmar = scanner.nextLine();
+                        if (confirmar.equals("S")) {
+                            manager.removeAllTasks();
+                        } else {
+                            System.out.println("Limpeza cancelada.");
+                        }
+                        break;
+                    case 0:
+                        System.out.println("Saindo...");
+                        scanner.close();
+                    default:
+                        System.out.println("Opção inválida");
+            }
+        }
     }
 
 }
